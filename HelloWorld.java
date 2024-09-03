3a6b0400be90655b7e2d53928cb5710b60bca8af
tramngyn <bvnguyen115@student.hbuhsd.edu>
2024-09-03 11:18:18 -0700

    unit 1.2

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,17 @@ public class HelloWorld
 {
   public static void main(String[] args)
   {
-    System.out.print("Hello world!");
+    System.out.println("Hello world!");
+    System.out.println("Hello world!");
+
+    int myAge;
+    myAge = 999;
+
+    double price = 4.99; //set price to 4.99
+    String myName = "Tram Nguyen"; 
+
+    boolean numApples = true;
+
+    System.out.println("Hello " + myName + ", it's nice to meet you");
   }
 }
