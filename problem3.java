0d70d1bfa685f85089a097f05d082e81b82818d3
tramngyn <bvnguyen115@student.hbuhsd.edu>
2024-09-03 11:17:16 -0700

    finished problems

--- problem3.java
+++ problem3.java
@@ -1,1 +1,11 @@
+public class Problem3
+{
+  public static void main(String[] args)
+  {
+    System.out.print("=========");
+    System.out.println("= 0   0 =   Java is Awesome!");
+    System.out.println("=   v   =");
+    System.out.println("=========");
+  }
+}
 
