15b5b1203e3ea2f78f295dc6f4a31478a2b40665
tramngyn <bvnguyen115@student.hbuhsd.edu>
2024-09-04 10:53:18 -0700

    finished assignment

--- problem2.java
+++ problem2.java
@@ -1,10 +1,10 @@
-public class Problem2
+public class problem2
 {
   public static void main(String[] args)
   {
-    System.out.print("Coding is fun!");
     System.out.println("Coding is fun!");
-    System.outprintln("Coding is fun!");
+    System.out.println("Coding is fun!");
+    System.out.println("Coding is fun!");
   }
 }
 
--- problem3.java
+++ problem3.java
@@ -1,8 +1,8 @@
-public class Problem3
+public class problem3
 {
   public static void main(String[] args)
   {
-    System.out.print("=========");
+    System.out.println("=========");
     System.out.println("= 0   0 =   Java is Awesome!");
     System.out.println("=   v   =");
     System.out.println("=========");
